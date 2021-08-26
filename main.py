ad854dfc9e34577de2cf80b4224bbfb75a19d8b3
mstanciu552 <mstanciu552@gmail.com>
2021-08-26 15:57:02 +0300

    added board passthrough

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 
+from Board import Board
+from piece_definitions import draw
+
+# TODO Make check_valid_move function
+
 pygame.init()
 
 WIDTH, HEIGHT = 800, 800
@@ -7,15 +12,24 @@ WIDTH, HEIGHT = 800, 800
 BLACK = (0, 0, 0)
 
 win = pygame.display.set_mode([WIDTH, HEIGHT])
+clock = pygame.time.Clock()
+
+board = Board(100)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            pass
 
     win.fill(BLACK)
 
+    board.draw(win)
+    draw(win, board)
+
     pygame.display.update()
+    clock.tick(30)
 
 pygame.quit()
